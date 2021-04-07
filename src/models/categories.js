0d20a570e2c0b64e2348d3cb7f21bcdbf74756bc
David Heinebäck <du222aa@student.lnu.se>
2021-04-07 12:37:09 +0200

    add categories schema

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,20 +1,55 @@
-// {
-//   id: 1,
-//   name: 'Kategori ett',
-//   searchurl: 'kategori-ett',
-//   description: 'Category description',
-//   subs: [
-//     {
-//       subid: 1,
-//       name: 'subcategory',
-//       searchurl: 'subcategory',
-//       description: 'Subcategory description'
-//     },
-//     {
-//       subid: 2,
-//       name: 'second category',
-//       searchurl: 'second-category',
-//       description: 'Subcategory description'
-//     }
-//   ]
-// }
+import mongoose from 'mongoose'
+
+// Create a schema.
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Categoryname is required.'],
+    unique: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required.']
+  },
+  id: {
+    type: Number,
+    unique: true
+  },
+  searchurl: {
+    type: String,
+    required: true
+  },
+  subs: {
+    type: Array
+  }
+}, {
+  timestamps: true,
+  versionKey: false,
+  toJSON: {
+    /**
+     * Performs a transformation of the resulting object to remove sensitive information.
+     *
+     * @param {object} doc - The mongoose document which is being converted.
+     * @param {object} ret - The plain object representation which has been converted.
+     */
+    transform: function (doc, ret) {
+      delete ret._id
+    },
+    virtuals: true // ensure virtual fields are serialized
+  }
+})
+
+/**
+ * Inserts a new category.
+ *
+ * @param {object} categoryData - ...
+ * @returns {Promise<Category>} - ...
+ */
+schema.statics.insert = async function (categoryData) {
+  const user = new Category(categoryData)
+  return user.save()
+}
+
+// Create a model using the schema.
+export const Category = mongoose.model('Category', schema)
