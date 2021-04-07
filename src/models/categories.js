f200935d30a5659bd8d3ad9d6de746e7fb142b7a
David Heinebäck <du222aa@student.lnu.se>
2021-04-07 10:20:37 +0200

    refactor: add schema to productsmodel

---
+++ src/models/categories.js
@@ -0,0 +1,20 @@
+// {
+//   id: 1,
+//   name: 'Kategori ett',
+//   searchurl: 'kategori-ett',
+//   description: 'Category description',
+//   subs: [
+//     {
+//       subid: 1,
+//       name: 'subcategory',
+//       searchurl: 'subcategory',
+//       description: 'Subcategory description'
+//     },
+//     {
+//       subid: 2,
+//       name: 'second category',
+//       searchurl: 'second-category',
+//       description: 'Subcategory description'
+//     }
+//   ]
+// }
---
+++ src/models/products.js
@@ -0,0 +1,87 @@
+import mongoose from 'mongoose'
+
+// Create a schema.
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Productname is required.'],
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: [true, 'description is required.']
+  },
+  detailedDescription: {
+    type: String
+  },
+  price: {
+    type: Number
+  },
+  discount: {
+    type: Number,
+    default: 1
+  },
+  productCategory: {
+    type: Array,
+    required: true
+  },
+  productSubCategory: {
+    type: Array,
+    required: true
+  },
+  inStock: {
+    type: Number
+  },
+  image: {
+    type: String
+  },
+  brand: {
+    type: String
+  },
+  itemNr: {
+    type: Number,
+    required: [true, 'item number is required.'],
+    unique: true,
+    trim: true
+  },
+  options: {
+    type: Array
+  },
+  reviews: {
+    type: Array
+  },
+  rating: {
+    type: Number
+  }
+}, {
+  timestamps: true,
+  versionKey: false,
+  toJSON: {
+    /**
+     * Performs a transformation of the resulting object to remove sensitive information.
+     *
+     * @param {object} doc - The mongoose document which is being converted.
+     * @param {object} ret - The plain object representation which has been converted.
+     */
+    transform: function (doc, ret) {
+      delete ret._id
+    },
+    virtuals: true // ensure virtual fields are serialized
+  }
+})
+
+/**
+ * Inserts a new product.
+ *
+ * @param {object} productData - ...
+ * @returns {Promise<Product>} - ...
+ */
+schema.statics.insert = async function (productData) {
+  const user = new Product(productData)
+  return user.save()
+}
+
+// Create a model using the schema.
+export const Product = mongoose.model('Product', schema)
