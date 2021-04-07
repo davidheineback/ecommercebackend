f84909db31bcca71a72f9b0d04232a2a2f71b50e
David Heinebäck <du222aa@student.lnu.se>
2021-04-07 12:39:59 +0200

    add postroutes

--- src/routes/api/v1/ecommerce-router.js
+++ src/routes/api/v1/ecommerce-router.js
@@ -1,6 +1,6 @@
 import express from 'express'
-// import { ProductsController } from '../../../controllers/api/products-controller.js'
-// import { CategoriesController } from '../../../controllers/api/categories-controller.js'
+import { ProductsController } from '../../../controllers/api/products-controller.js'
+import { CategoriesController } from '../../../controllers/api/categories-controller.js'
 // import { FetchController } from '../../../controllers/api/fetch-controller.js'
 // import { TokenController } from '../../../controllers/api/token-controller.js'
 
@@ -8,8 +8,10 @@ export const router = express.Router()
 
 // const fetchController = new FetchController()
 // const tokenController = new TokenController()
-// const productsController = new ProductsController()
-// const categoriesController = new CategoriesController()
+const productsController = new ProductsController()
+const categoriesController = new CategoriesController()
 
 // Map HTTP verbs and route paths to controller actions.
-router.route('/category/:id').get()
+router.route('/admin/addcategory').post(categoriesController.addNewCategory)
+router.route('/admin/addsubcategory').post(categoriesController.addNewSubCategory)
+router.route('/admin/addproduct').post(productsController.addNewProduct)
