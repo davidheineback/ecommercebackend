e6c990c1d2ae15745f627455444d0bb9d91beef5
David Heinebäck <du222aa@student.lnu.se>
2021-04-07 09:20:11 +0200

    add: fetch controller

---
+++ src/controllers/api/fetch-controller.js
@@ -0,0 +1,28 @@
+// import fetch from 'node-fetch'
+
+/**
+ * Encapsulates a controller.
+ */
+export class FetchController {
+  /**
+   * Get products by category.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   * @returns {Error} - Returns a error if user validation is failed.
+   */
+  async getProductsByCategory (req, res, next) {
+  }
+
+  /**
+   * Get a product specified by id.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   * @returns {Error} - Returns a error if user validation is failed.
+   */
+  async getProdctWithId (req, res, next) {
+  }
+}
