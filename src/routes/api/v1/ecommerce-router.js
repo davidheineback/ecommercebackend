7267ee56ba14b02dc6ed41414cce9a22bc8c14ef
David Heinebäck <du222aa@student.lnu.se>
2021-04-08 15:41:46 +0200

    add: get routes

--- src/routes/api/v1/ecommerce-router.js
+++ src/routes/api/v1/ecommerce-router.js
@@ -1,17 +1,22 @@
 import express from 'express'
 import { ProductsController } from '../../../controllers/api/products-controller.js'
 import { CategoriesController } from '../../../controllers/api/categories-controller.js'
-// import { FetchController } from '../../../controllers/api/fetch-controller.js'
+import { FetchController } from '../../../controllers/api/fetch-controller.js'
 // import { TokenController } from '../../../controllers/api/token-controller.js'
 
 export const router = express.Router()
 
-// const fetchController = new FetchController()
+const fetchController = new FetchController()
 // const tokenController = new TokenController()
 const productsController = new ProductsController()
 const categoriesController = new CategoriesController()
 
-// Map HTTP verbs and route paths to controller actions.
+// Get routes::
+router.route('/category/:id').get(fetchController.getIdFromMainName, fetchController.getProductsByCategoryId)
+router.route('/subcategory/:id').get(fetchController.getIdFromSubName, fetchController.getProductsByCategoryId)
+router.route('/product/:id').get(fetchController.getProdctWithId)
+
+// Post routes::
 router.route('/admin/addcategory').post(categoriesController.addNewCategory)
 router.route('/admin/addsubcategory').post(categoriesController.addNewSubCategory)
 router.route('/admin/addproduct').post(productsController.addNewProduct)
