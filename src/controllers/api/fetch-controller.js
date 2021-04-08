8c90cba68dd82824c568b4ec7cc84c3783280981
David Heinebäck <du222aa@student.lnu.se>
2021-04-08 16:33:53 +0200

    refactor fetchcontroller to handle get method

--- src/controllers/api/fetch-controller.js
+++ src/controllers/api/fetch-controller.js
@@ -1,18 +1,79 @@
-// import fetch from 'node-fetch'
+import { Product } from '../../models/products.js'
+import { SubCategory } from '../../models/subcategories.js'
+import { Category } from '../../models/categories.js'
+import { filterOutput } from './method-controller.js'
 
 /**
  * Encapsulates a controller.
  */
 export class FetchController {
   /**
-   * Get products by category.
+   * Get products by main or subcategory id.
    *
    * @param {object} req - Express request object.
    * @param {object} res - Express response object.
    * @param {Function} next - Express next middleware function.
    * @returns {Error} - Returns a error if user validation is failed.
    */
-  async getProductsByCategory (req, res, next) {
+  async getProductsByCategoryId (req, res, next) {
+    try {
+      let rawProducts
+      if (req.mainId) {
+        rawProducts = await Product.find({ productCategory: req.mainId })
+      } else {
+        rawProducts = await Product.find({ productSubCategory: req.subId })
+      }
+      if (rawProducts) {
+        const products = rawProducts.map(rawProduct => filterOutput(rawProduct))
+        return res.status(200).send(products)
+      }
+      return res.sendStatus(201)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Get Id for a main category.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   * @returns {Error} - Returns a error if user validation is failed.
+   */
+  async getIdFromMainName (req, res, next) {
+    try {
+      const maincategory = await Category.findOne({ searchurl: req.params.id })
+      console.log(maincategory)
+      if (!maincategory) {
+        res.sendStatus(404)
+      }
+      req.mainId = maincategory.id
+      next()
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   * Get Id for a sub category.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   * @returns {Error} - Returns a error if user validation is failed.
+   */
+  async getIdFromSubName (req, res, next) {
+    try {
+      const subcategory = await SubCategory.findOne({ searchurl: req.params.id })
+      if (!subcategory) {
+        res.sendStatus(404)
+      }
+      req.subId = subcategory.subid
+      next()
+    } catch (error) {
+      next(error)
+    }
   }
 
   /**
@@ -24,5 +85,15 @@ export class FetchController {
    * @returns {Error} - Returns a error if user validation is failed.
    */
   async getProdctWithId (req, res, next) {
+    try {
+      const rawProduct = await Product.findOne({ itemNr: req.params.id })
+      if (rawProduct) {
+        const product = filterOutput(rawProduct)
+        return res.status(200).send(product)
+      }
+      return res.status(404).send(`Couldn't find any products with id: ${req.params.id}`)
+    } catch (error) {
+      next(error)
+    }
   }
 }
