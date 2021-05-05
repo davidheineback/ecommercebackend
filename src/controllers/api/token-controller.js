c3051e2de2d15b1a13668cccebcf95bb2fe78cfc
David Heinebäck <du222aa@student.lnu.se>
2021-05-05 10:53:12 +0200

    refactor: add register and login routes

--- src/controllers/api/token-controller.js
+++ src/controllers/api/token-controller.js
@@ -1,9 +1,46 @@
 import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
+import { User } from '../../models/user.js'
 
 /**
  * Encapsulates a controller.
  */
 export class TokenController {
+  /**
+   * Authenticates a user.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async login (req, res, next) {
+    try {
+      const user = await User.authenticate(req.body.username, req.body.password)
+
+      const payload = {
+        sub: user.username
+      }
+
+      // Create the access token with the shorter lifespan.
+      const accessToken = jwt.sign(payload, Buffer.from(process.env.SIGN_TOKEN_SECRET, 'base64'), {
+        algorithm: 'RS256',
+        expiresIn: process.env.ACCESS_TOKEN_LIFE
+      })
+
+      res
+        .status(200)
+        .json({
+          access_token: accessToken
+        })
+    } catch (error) {
+      // Authentication failed.
+      const err = createError(401)
+      err.innerException = error
+
+      next(err)
+    }
+  }
+
   /**
    * Authorize the user by validating the token.
    *
@@ -20,7 +57,7 @@ export class TokenController {
         return res.sendStatus(401)
       }
 
-      jwt.verify(authToken, Buffer.from(process.env.ACCESS_TOKEN_SECRET, 'base64'), (error, user) => {
+      jwt.verify(authToken, Buffer.from(process.env.SIGN_TOKEN_SECRET, 'base64'), (error, user) => {
         if (error) {
           return res.sendStatus(403)
         }
@@ -31,6 +68,39 @@ export class TokenController {
       next()
     } catch (error) {
       next(error)
+    }
+  }
+
+  /**
+   * Registers a user.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async register (req, res, next) {
+    try {
+      const user = await User.insert({
+        username: req.body.username,
+        password: req.body.password
+      })
+
+      res
+        .status(201)
+        .json({ id: user.id })
+    } catch (error) {
+      let err = error
+
+      if (err.code === 11000) {
+        // Duplicated keys.
+        err = createError(409)
+        err.innerException = error
+      } else if (error.name === 'ValidationError') {
+        // Validation error(s).
+        err = createError(400)
+        err.innerException = error
+      }
+      next(err)
     }
   }
 }
