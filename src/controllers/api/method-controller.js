a2a2c4f3c120c339dea66f68ee17790e3512cd22
David Heinebäck <du222aa@student.lnu.se>
2021-05-12 11:50:47 +0200

    refactor: created method for accessToken cand refreshToken check, extracted creation of jwt from token controller to methods

--- src/controllers/api/method-controller.js
+++ src/controllers/api/method-controller.js
@@ -1,6 +1,8 @@
 import { Category } from '../../models/categories.js'
 import { SubCategory } from '../../models/subcategories.js'
 import { Product } from '../../models/products.js'
+import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
 
 /**
  * Set new id.
@@ -109,4 +111,78 @@ export async function findCategoryIdByName (type, mainCategoryname, subCategoryn
     }
   }
   return sub
+}
+
+/**
+ * Handles refreshtoken.
+ *
+ * @param {object} req - represents request object.
+ * @param {object} user - represents request object.
+ * @returns {string} - A string with a token || a Error.
+ */
+export function accessCheckCall (req, user) {
+  jwt.verify(req.body.access_token, req.secret,
+    async (error) => {
+      if (error) {
+        if (req.body.refresh_token) {
+          req.body.access_token = refreshCall(req.body, user)
+        } else {
+          return createError(403)
+        }
+      }
+    })
+  return req.body.access_token
+}
+
+/**
+ * Handles refreshtoken.
+ *
+ * @param {object} req - represents request object.
+ * @param {object} user - represents request object.
+ * @returns {status} - Returns a status.
+ */
+function refreshCall (req, user) {
+  if (user.refreshToken === req.refresh_token) {
+    const newToken = jwt.verify(req.refresh_token, process.env.REFRESH_TOKEN_SECRET,
+      (error, user) => {
+        if (error) {
+          return createError(403)
+        }
+        const payload = {
+          sub: user.username,
+          isAdmin: user.isAdmin
+        }
+        const token = getAccessToken(payload)
+        return token
+      })
+    return newToken
+  } else {
+    return createError(401)
+  }
+}
+
+/**
+ * Signs a jwt accesstoken.
+ *
+ * @param {object} payload - a payload to sign in jwt accesstoken
+ * @returns {*} - A signed jwt accesstoken.
+ */
+export function getAccessToken (payload) {
+  return (jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    algorithm: 'HS256',
+    expiresIn: process.env.ACCESS_TOKEN_LIFE
+  }))
+}
+
+/**
+ * Signs a jwt refresh token.
+ *
+ * @param {object} payload - a payload to sign in jwt refreshtoken
+ * @returns {*} - A signed jwt accesstoken.
+ */
+export function getRefreshToken (payload) {
+  return (jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
+    algorithm: 'HS256',
+    expiresIn: process.env.REFRESH_TOKEN_LIFE
+  }))
 }
